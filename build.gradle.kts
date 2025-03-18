452d3afedf681c3197c87333252763755f06e90a
Milan4321123 <136030456+Milan4321123@users.noreply.github.com>
2025-03-18 15:42:53 +0100

    Update build.gradle.kts
    config update

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,14 @@ node{
 }
 
 tasks.register<NpmTask>("appNpmInstall"){
-    setWorkingDir(file("${project.projectDir}"))
+    setWorkingDir(file("${project.projectDir}/frontend"))  // Make sure this points to frontend
     setArgs(listOf("install", "react-folder-tree@5.1.1", "--force"))
 }
+
 tasks.register<NpmTask>("appNpmBuild") {
-    setWorkingDir(file("${project.projectDir}"))
+    setWorkingDir(file("${project.projectDir}/frontend"))  // Make sure this points to frontend
     setArgs(listOf("run", "build"))
 }
-
 
 tasks.named("appNpmBuild"){
     dependsOn(tasks["appNpmInstall"])
